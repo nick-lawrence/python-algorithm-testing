16d36abc393e52766d5acb3f96760cc8d857f473
nick-lawrence <nicklawrence.design@gmail.com>
2018-12-14 09:22:51 -0800

    first commit

---
+++ bubble_sort.py
@@ -0,0 +1,21 @@
+#An Example of a very rudimentary bubble sorting algorithm
+
+def bubbleSort(dataset):
+
+  for i in range(len(dataset) -1, 0, -1):
+    for j in range(i):
+      if dataset[j] > dataset[j+1]:
+        temp = dataset[j]
+        dataset[j] = dataset[j+1]
+        dataset[j+1] = temp
+
+    print("Current state: ", dataset)
+
+
+def main():
+  list1 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
+  bubbleSort(list1)
+  print("Results: ", list1)
+
+if __name__ == "__main__":
+  main()
